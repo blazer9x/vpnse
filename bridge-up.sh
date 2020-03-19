9726fb3c411d5c7cdf698dcb7cc1dc536f859782
blazer9x <42729130+blazer9x@users.noreply.github.com>
2020-03-20 01:32:02 +0700

    Create bridge-up.sh

---
+++ bridge-up.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IP=192.168.80.1
+DHCPS=192.168.80.10,192.168.80.200
+
+cat << DNSM > /etc/dnsmasq.conf
+interface=tap_1
+dhcp-range=tap_1,$DHCPS,24h
+dhcp-option=tap_1,3,$IP
+cache-size=1000
+DNSM
+
+ifconfig tap_1 $IP
+systemctl restart dnsmasq
